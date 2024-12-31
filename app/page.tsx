cbbf7639fb89789b1850b4c71f8a6e768dd51fbc
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2024-12-31 17:34:47 +0200

    feat: redirect signed-in users to the dashboard on entry point

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client"
 import SignIn from "@/app/auth/sign-in/page";
+import { useRouter } from "next/router";
 
 export default function EntryPoint() {
+  // redirect the user to the dashboard if they are already signed in
+  if(typeof window !== "undefined") {
+    window.location.href = "/dashboard";
+  }
+
   return (
     <section>
     </section>
