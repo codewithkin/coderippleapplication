51e39937ae91abb621c93503e93b3fe6bbf9df50
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2024-12-31 16:09:16 +0200

    refactor GitHub authentication provider configuration to simplify redirect URI handling

--- auth.config.ts
+++ auth.config.ts
@@ -4,13 +4,7 @@ import { redirect } from "next/dist/server/api-utils"
  
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub({
-    authorization: {
-      params: {
-        redirect_uri: "https://app.coderipple.live/api/auth/callback/github",
-      },
-    },
-  })],
+  providers: [GitHub],
   callbacks: {
     async redirect({ url, baseUrl }) {
       // Example: Redirect to a specific URL after sign-in
