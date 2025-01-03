66d53e0fa3e626248d224d34884f27ed59fc1ae8
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-03 06:35:01 +0200

    fix: update productPriceId for production environment in handleCreditsPurchase

--- lib/handleCreditsPurchase.ts
+++ lib/handleCreditsPurchase.ts
@@ -6,7 +6,7 @@ export const handlePayment = async (event: React.FormEvent) => {
 
     // Initiate a new payment
         var checkout = await api.checkouts.custom.create({
-            productPriceId: "d72f0af8-0d49-4ac5-9d57-44dad5de793b", // TODO: Remove this
+            productPriceId: process.env.NODE_ENV === "production" ? "0c7c81c2-1321-4e5e-85bb-6031dd79cef9" : "d72f0af8-0d49-4ac5-9d57-44dad5de793b", // TODO: Remove this
             successUrl: "http://localhost:3000/confirmation?checkout_id={CHECKOUT_ID}",
         });
 
